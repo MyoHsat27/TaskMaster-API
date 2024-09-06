60cf13be6329654299419c2419e58f6c98f4c169
Myo Hsat <hsatmyo@gmail.com>
2024-09-07 02:01:00 +0700

    <add>[user]: add password hasher and validate exisiting user

--- src/controller/v1/userController.ts
+++ src/controller/v1/userController.ts
@@ -2,17 +2,28 @@ import { Request, Response } from "express";
 import { signUpValidation } from "../../validations/signup.js";
 import { validate } from "../../utils/zodValidation.js";
 import { HttpBadRequestHandler } from "../../utils/httpResponseHandler.js";
+import { userService } from "../../services/v1/userService.js";
+import { hashPassword } from "../../utils/passwordManager";
 
+const { findOne, create } = userService();
 export const userController = () => {
     const login = async (req: Request, res: Response) => {};
 
     const register = async (req: Request, res: Response) => {
         try {
             const body = req.body;
-            const validationResult = validate(body, signUpValidation);
 
+            // Validate registration input
+            const validationResult = validate(body, signUpValidation);
             if (validationResult) {
                 return HttpBadRequestHandler(res, validationResult);
+            }
+
+            // Check the existing user
+            const { username, email, password } = body;
+            const user = await findOne({ email });
+            if (user) {
+                return HttpBadRequestHandler(res, "user already exists");
             }
         } catch (error: any) {
             return HttpBadRequestHandler(res, { error: error.message });
--- src/services/v1/userService.ts
+++ src/services/v1/userService.ts
@@ -2,6 +2,11 @@ import User from "../../models/user.js";
 import { UserCreateObject } from "../../types/user.js";
 
 export const userService = () => {
+    const findOne = async (param: Record<string, string>) => {
+        const user = await User.findOne(param);
+        return user;
+    };
+
     const create = (user: UserCreateObject) => {
         const newUser = new User(user);
         const savedUser = newUser.save();
@@ -9,5 +14,5 @@ export const userService = () => {
         return savedUser;
     };
 
-    return { create };
+    return { findOne, create };
 };
