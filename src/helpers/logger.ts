f96baa7cbb00f30d813a40e173b3a087d9fb8a94
Myo Hsat <hsatmyo@gmail.com>
2024-09-08 05:50:26 +0700

    <add>[logging]:add log file and logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,11 +1,23 @@
-import logger from "pino";
-import dayjs from "dayjs";
+import pino from "pino";
+import { createStream } from "rotating-file-stream";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const log = logger({
-    base: {
-        pid: false
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`
+// Get the equivalent of __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const logFilePath = path.resolve(__dirname, "../logs/app.log");
+const logStream = createStream("app.log", {
+    size: "10M",
+    interval: "1d",
+    path: path.resolve(__dirname, "../logs")
 });
+const logger = pino(
+    {
+        level: process.env.PINO_LOG_LEVEL || "info"
+    },
+    logStream
+);
 
-export default log;
+export default logger;
